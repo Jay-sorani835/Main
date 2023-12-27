75414895b1594636c58b8afd2201b8bed1322387
Jay-sorani835 <148628735+Jay-sorani835@users.noreply.github.com>
2023-12-27 17:57:59 +0530

    Add files via upload

---
+++ Practise/m-2(3)/pattern/4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,j;
+    for(i = 0;i < 6;i++){
+        for(j = 0 ; j < i;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    for(i = 6;i > 0;i--){
+        for(j = 0;j < i;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
