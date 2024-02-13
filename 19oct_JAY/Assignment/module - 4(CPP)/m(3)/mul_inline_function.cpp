6224ee26658a333c8cbd8127bd48b3bb42268439
Jay-sorani835 <148628735+Jay-sorani835@users.noreply.github.com>
2024-02-13 15:54:39 +0530

    Update mul_inline_function.cpp

--- 19oct_JAY/Assignment/module - 4(CPP)/m(3)/mul_inline_function.cpp
+++ 19oct_JAY/Assignment/module - 4(CPP)/m(3)/mul_inline_function.cpp
@@ -10,10 +10,14 @@ void get(){
     cin>>b;
 }
 inline int dm(){return a*b;}
+inline int cu(){return (a*a*a);}
+inline int cu1(){return (b*b*b);}
 };
 int main(){
     demo d1;
     d1.get();
     cout<<"The multiplication of two numbers is : "<<d1.dm()<<endl;
+    cout<<"The cubic value of first number is : "<<d1.cu()<<endl;
+    cout<<"The cubic value of second number is : "<<d1.cu1()<<endl;
     return 0;
 }
